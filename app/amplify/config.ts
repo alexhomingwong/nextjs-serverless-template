1d76899237df1de95314e8d059521469e5de31dc
alexhomingwong <wonghoming.home@gmail.com>
2024-01-28 23:56:52 +0000

    added global error boundary and also check to make sure the cognito env are set

--- app/amplify/config.ts
+++ app/amplify/config.ts
@@ -1,3 +1,10 @@
+if (
+  !process.env.NEXT_PUBLIC_COGNITO_USERPOOL_ID ||
+  !process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID
+) {
+  throw new Error("Missing Cognito settings");
+}
+
 export const config = {
   Auth: {
     Cognito: {
